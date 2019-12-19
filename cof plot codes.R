b31e9c0f25855741547637ada3c49cf68161de5e
ecomohitpande <50360364+ecomohitpande@users.noreply.github.com>
2019-12-19 12:17:23 +0530

    Update cof plot codes.R

--- cof plot codes.R
+++ cof plot codes.R
@@ -46,18 +46,34 @@ dwplot(by_trans,
                       labels = c("Automatic", "Manual"))
 
 ### plot the normal distribution 
-dwplot(by_trans, 
-       vline = geom_vline(xintercept = 0, colour = "grey60", linetype = 2)) + # plot line at zero _behind_ coefs
-    theme_bw() + xlab("Coefficient Estimate") + ylab("") +
-    ggtitle("Predicting Gas Mileage by Transmission Type") +
-    theme(plot.title = element_text(face="bold"),
-          legend.position = c(0.007, 0.01),
-          legend.justification = c(0, 0),
+
+by_transmission_brackets <- list(c("Overall", "Weight", "Weight"), 
+                       c("Engine", "Cylinders", "Horsepower"),
+                       c("Transmission", "Gears", "Gears"))
+        
+{mtcars %>%
+    split(.$am) %>%
+    purrr::map(~ lm(mpg ~ wt + cyl + disp + hp + gear, data = .x)) %>%
+    dwplot(style = "distribution") %>%
+    relabel_predictors(wt = "Weight",
+                         cyl = "Cylinders",
+                         disp = "Displacement",
+                         hp = "Horsepower",
+                         gear = "Gears") +
+    theme_bw() + xlab("Coefficient") + ylab("") +
+    geom_vline(xintercept = 0, colour = "grey60", linetype = 2) +
+    theme(legend.position = c(.995, .99),
+          legend.justification = c(1, 1),
           legend.background = element_rect(colour="grey80"),
           legend.title.align = .5) +
-    scale_colour_grey(start = .3, end = .7,
+    scale_colour_grey(start = .8, end = .4,
                       name = "Transmission",
-                      breaks = c(0, 1),
-                      labels = c("Automatic", "Manual"))
-
-
+                      breaks = c("Model 0", "Model 1"),
+                      labels = c("Automatic", "Manual")) +
+    scale_fill_grey(start = .8, end = .4,
+                    name = "Transmission",
+                    breaks = c("Model 0", "Model 1"),
+                    labels = c("Automatic", "Manual"))} %>%
+    add_brackets(by_transmission_brackets) +
+    ggtitle("Predicting Gas Mileage by Transmission Type") +
+    theme(plot.title = element_text(face = "bold", hjust = 0.5))
